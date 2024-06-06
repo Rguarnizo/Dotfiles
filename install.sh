2c28cffcc30ea0d0322658c94ec2de29bfb95934
rguarnizo <rguarnizo@unal.edu.co>
2024-06-05 21:58:30 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-mkdir ~/android-sdk
-cd ~/android-sdk
-wget https://dl.google.com/android/repository/commandlinetools-mac-9477386_latest.zip -O cli-tools-android.zip
-unzip cli-tools-android.zip
-tools/bin/sdkmanager --update
+ln -s ~/.dotfiles/.* ~/
+
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew bundle --file ~/.dotfiles/Brewfile
