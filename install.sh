2e9a37aa3282757b3f8df9831894583821cb9e31
Rubén Darío Guarnizo Martinez <rguarnizo@MacBook-Air-de-Ruben.local>
2024-06-09 08:38:36 -0500

    Add Themes of Iterm2

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
-ln -s ~/.dotfiles/.* ~/
+ln -s ~/dotfiles/.* ~/
 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew bundle --file ~/.dotfiles/Brewfile
+brew bundle --file ~/dotfiles/Brewfile
+
+
+defaults write com.apple.dock "autohide" -bool "true" && killall Dock
+defaults write com.apple.finder "QuitMenuItem" -bool "true" && killall Finder
+defaults -currentHost write -globalDomain com.apple.mouse.tapBehavior -int 
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move focus to next window" '⌥\t'
+
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
