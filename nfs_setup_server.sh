1d3b88bd1cc834539fbc67c66ab5e3ae842ca9eb
Ruben Dario <rguarnizo@unal.edu.co>
2025-08-19 19:56:55 -0500

    NFS script Linux

--- nfs_setup_server.sh
+++ nfs_setup_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Configuración rápida de NFS en Steam Deck para compartir /home/deck/Emulation/roms
 #
-PC_IP="192.168.5.105"  # <-- Cambia esto por la IP de tu PC con Windows/WSL
+PC_IP="*"  # <-- Cambia esto por la IP de tu PC con Windows/WSL
 #
 echo "1. Dando permisos a la carpeta..."
 sudo mkdir -p /home/deck/Emulation/roms
@@ -12,7 +12,8 @@ sudo pacman -Sy --noconfirm nfs-utils
 #
 echo "3. Configurando /etc/exports..."
 sudo bash -c "cat > /etc/exports <<EOF
-/home/deck/Emulation/roms ${PC_IP}(rw,sync,no_subtree_check,no_root_squash)
+/home/deck/Emulation/roms ${PC_IP}(rw,sync,no_subtree_check,no_root_squash,insecure)
+/home/deck/Emulation/bios ${PC_IP}(rw,sync,no_subtree_check,no_root_squash,insecure)
 EOF"
 #
 echo "4. Activando servicios NFS..."
@@ -21,6 +22,7 @@ sudo systemctl enable --now nfs-server rpcbind nfs-idmapd
 echo "5. Recargando exportaciones..."
 sudo exportfs -ra
 sudo exportfs -v
+export PATH=$HOME/.local/bin:$PATH
 #
 echo "6. Configurando firewall..."
 sudo firewall-cmd --permanent --add-service=nfs
@@ -31,4 +33,5 @@ sudo firewall-cmd --reload
 echo "✅ NFS configurado. Ahora desde WSL ejecuta:"
 echo "sudo mkdir -p /mnt/roms"
 echo "sudo mount -t nfs ${HOSTNAME}:/home/deck/Emulation/roms /mnt/roms"
+echo "sudo mount -t nfs ${HOSTNAME}:/home/deck/Emulation/bios /mnt/bios"
 #
